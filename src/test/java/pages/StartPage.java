44dbd7363ed7e307be96d4467762579e744d56b5
MariaYanu <yanushkevichmariam@gmail.com>
2022-12-03 20:42:55 +0300

    code clean complete

--- src/test/java/pages/StartPage.java
+++ src/test/java/pages/StartPage.java
@@ -8,13 +8,10 @@ public class StartPage extends StartSubMenuPage{
 
     @FindBy(xpath = "//div[@id='main']/h2")
     private WebElement h2Header;
-    private final String H2_HEADER_TEXT = "Welcome to 99 Bottles of Beer";
 
     public StartPage(WebDriver driver) {
         super(driver);
     }
-
-    public String getH2_HEADER_TEXT(){return H2_HEADER_TEXT;}
 
     public String getH2HeaderText(){
 
